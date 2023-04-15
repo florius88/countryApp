3c14ab949d4555026487f6f76a4ec64604fd2a3d
florius88 <florsantosbueno@gmail.com>
2023-04-15 20:13:36 +0200

    CountriesRoutingModule y LazyLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,6 +20,11 @@ const routes: Routes = [
         component: ContactPageComponent
     },
     {
+        path: 'countries',
+        loadChildren: () => import('./countries/countries.module')
+            .then( m => m.CountriesModule )
+    },
+    {
         path: '**',
         redirectTo: ''
     },
